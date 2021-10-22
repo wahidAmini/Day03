3053ee7612a33bf9a0edba459d1d37216ac7ccef
apprenant <colas.tony47@gmail.com>
2021-10-22 15:30:50 +0200

    ex03 and ex04 finished

--- ex04/backup.sh
+++ ex04/backup.sh
@@ -1,2 +1,3 @@
 # script for backup the Images folder inside Documents then inside backup folder
-30 07 * * * tar -czf /home/apprenant/Documents/backup/"backup_`date +%a_%d_%b_%Y_%H_%M_%S_%Z`".tar.gz ~/Images
+# 30 07 * * * tar -czf /home/apprenant/Documents/backup/"backup_`date +%a_%d_%b_%Y_%H_%M_%S_%Z`".tar.gz ~/Images
+30 7 * * * tar -czf /home/apprenant/Documents/backup/"backup_$(date | sed -e 's/[,: ]/__/g' -e 's/__/_/g')".tar.gz Images
